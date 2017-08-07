5c559330d37d7c83ab739db6bfc251c8f6587297
LionsWrath <caio.tonetti@gmail.com>
2017-08-06 22:03:54 -0300

    Oi Chicha

--- process.py
+++ process.py
@@ -1,13 +1,14 @@
 from random import randint, random
 
+#Whats missing:
+#   - PID
+
 BLOCKED     = 2
 EXECUTING   = 1
 READY       = 0
 
 class Process:
-    def __init__(self, manager, priority, type):
-        self.manager = manager
-        self.priority = priority
+    def __init__(self, type):
         self.type = type
 
         self.generate_times()
@@ -39,6 +40,9 @@ class Process:
             self.itime = randint(1, 3) * 3
             self.chance = 0.5
 
+    def preempt(self):
+        self.state = READY
+
     def update(self):
         
         if self.state == EXECUTING:
@@ -47,37 +51,104 @@ class Process:
                 self.state == BLOCKED
             else:
                 # Continue processing
-                self.pwait = self.pwait + 1
+                self.pwait += 1
 
                 if self.pwait == self.ptime:
                     self.pwait = 0
                     self.state = READY
 
         if self.state == BLOCKED:
-            self.iwait = self.iwait + 1
+            self.iwait += 1
 
             if self.iwait == self.itime:
                 self.iwait = 0
                 self.state = READY
 
+class ControlBlock:
+
+    def __init__(self, process, PID, priority, quantum):
+        self.process            = process
+        self.PID                = PID
+        self.quantum            = quantum
+        self.priority           = priority
+
+        self.default_priority   = priority
+        self.default_quantum    = quantum
+
+    def decrease_priority():
+        self.priority -= 1 
+
+    def decrease_quantum():
+        self.quantum -= 1
+
+    def reset():
+        self.priority = self.default_priority
+        self.quantum = self.default_quantum
+    
+    def get_status():
+        return self.process.get_status()
+
+    def update():
+        self.process.update()
+
 class ProcessManager:
 
-    def __init__(self, psize, schedule):
+    def __init__(self, psize, scheduling_type):
         self.psize = psize
+        self.scheduling_type = scheduling_type
 
         self.pslot = False
         self.islot = False
+
         self.pbuffer = []
         self.ibuffer = []
+
+    def add_process(self, type, priority, quantum):
+        p = Process(self, type)
+        c = ControlBlock(p, priority, quantum)
+
+        self.pbuffer.append(c)
+
+#---------------------------------------------------------------------------------------------------
+
+    def round_robin(self):
+        pass
+
+    def round_robin_priority(self):
+        pass
+
+    def priority(self):
+        pass
+
+    def dynamic_priority(self):
+        pass
+
+    def lottery(self):
+        pass
+
+    def schedule(self):
+        sched = { 
+                'ROUND ROBIN'               : self.round_robin,
+                'ROUND ROBIN (PRIORITY)'    : self.round_robin_priority,
+                'PRIORITY'                  : self.priority,
+                'DYNAMIC PRIORITY'          : self.dynamic_priority,
+                'LOTTERY'                   : self.lottery
+                }[self.scheduling_type]
+
+        return sched()
+
+#---------------------------------------------------------------------------------------------------
 
     def resolve_executing(self):
         s = self.pslot.get_status() 
         
         if s == BLOCKED:
+            self.pslot.reset()
             self.ibuffer.append(self.pslot)
             self.pslot = False
 
         if s == READY:
+            self.pslot.reset()
             self.pbuffer.append(self.pslot)
             self.pslot = False
 
@@ -89,7 +160,7 @@ class ProcessManager:
             self.islot = False
 
     def update(self):
-        # Begin updating the main processes
+        # Update the main processes
         if self.pslot:
             self.pslot.update()
             self.resolve_executing()
@@ -98,4 +169,5 @@ class ProcessManager:
             self.islot.update()
             self.resolve_blocked()
 
-         
+        schedule()
+
